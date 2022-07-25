e3bea887e34a77ce9bba258cc3af131ff01be2c1
Andrea Morabito <andrea.morabito@pagopa.it>
2022-07-25 15:42:56 +0200

    [RTD-193] Creation of endpoints for store and retrieve data

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/controller/RestControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/controller/RestControllerImpl.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.rtd.ms.rtdmssenderauth.controller;
 
+import it.gov.pagopa.rtd.ms.rtdmssenderauth.service.SenderAuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @ResponseBody
 @Slf4j
+@RequiredArgsConstructor
 public class RestControllerImpl implements DefaultRestController {
 
+  private final SenderAuthService authService;
+
   @Override
-  public void healthCheck() {
-    //Return OK if the service is reachable
+  public String getSenderCode(String apiKey) {
+    log.info("Getting SenderCode from apikey={}", apiKey);
+    return authService.getSenderCode(apiKey);
+  }
+
+  @Override
+  public void saveApiKey(String senderCode, String apiKey) {
+    log.info("Saving senderCode:{} and apiKey:{}", senderCode, apiKey);
+    authService.saveApiKey(senderCode, apiKey);
   }
 }
--- src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/repository/SenderAuthRepository.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/repository/SenderAuthRepository.java
@@ -1,8 +1,14 @@
 package it.gov.pagopa.rtd.ms.rtdmssenderauth.repository;
 
 import it.gov.pagopa.rtd.ms.rtdmssenderauth.model.SenderData;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface SenderAuthRepository extends MongoRepository<SenderData, String> {
 
+  Optional<SenderData> findByApiKey(String apiKey);
+
+  Optional<SenderData> findBySenderCode(String senderCode);
+
+  void deleteBySenderCode(String senderCode);
 }
