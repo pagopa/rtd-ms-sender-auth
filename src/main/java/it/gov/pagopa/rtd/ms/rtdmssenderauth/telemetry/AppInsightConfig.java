b2df3a9b278f7bf44706edd21603e4923886654d
petretiandrea <petretiandrea@gmail.com>
2023-09-19 14:41:20 +0200

    using appinsight env variable to set role

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/telemetry/AppInsightConfig.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/telemetry/AppInsightConfig.java
@@ -6,23 +6,23 @@ import io.opentelemetry.instrumentation.mongo.v3_1.MongoTelemetry;
 import io.opentelemetry.sdk.logs.export.LogRecordExporter;
 import io.opentelemetry.sdk.metrics.export.MetricExporter;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration for AppInsight OpenTelemetry.
+ * Connection string will automatically take from environment variable APPLICATIONINSIGHTS_CONNECTION_STRING
+ */
 @Configuration
 @ConditionalOnProperty(value = "applicationinsights.enabled", matchIfMissing = false)
 public class AppInsightConfig {
 
   private final AzureMonitorExporterBuilder azureMonitorExporterBuilder;
 
-  public AppInsightConfig(
-      @Value("${applicationinsights.connectionstring}") String appInsightConnectionString
-  ) {
-    this.azureMonitorExporterBuilder = new AzureMonitorExporterBuilder()
-        .connectionString(appInsightConnectionString);
+  public AppInsightConfig() {
+    this.azureMonitorExporterBuilder = new AzureMonitorExporterBuilder();
   }
 
   @Bean
@@ -44,6 +44,7 @@ public class AppInsightConfig {
   MongoClientSettingsBuilderCustomizer mongoOpenTelemetryBridge(
       OpenTelemetry openTelemetry
   ) {
-    return (clientSettingsBuilder) -> clientSettingsBuilder.addCommandListener(MongoTelemetry.builder(openTelemetry).build().newCommandListener());
+    return clientSettingsBuilder -> clientSettingsBuilder
+        .addCommandListener(MongoTelemetry.builder(openTelemetry).build().newCommandListener());
   }
 }
