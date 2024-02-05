865f1ffa7d2e273f0940c75c809b9063615b94e7
petretiandrea <petretiandrea@gmail.com>
2024-02-05 12:31:11 +0100

    added heartbeat logger

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/MongoConfig.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/MongoConfig.java
@@ -1,6 +1,11 @@
 package it.gov.pagopa.rtd.ms.rtdmssenderauth;
 
+import com.mongodb.event.ServerHeartbeatFailedEvent;
+import com.mongodb.event.ServerHeartbeatStartedEvent;
+import com.mongodb.event.ServerHeartbeatSucceededEvent;
+import com.mongodb.event.ServerMonitorListener;
 import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +23,7 @@ public class MongoConfig {
   @Bean
   public MongoClientSettingsBuilderCustomizer mongoConnectionPoolConfiguration() {
     return builder -> builder
+        .applyToServerSettings(srv -> srv.addServerMonitorListener(new HeartBeatLogger()))
         .applyToConnectionPoolSettings(pool -> {
           pool.maxWaitTime(COSMOS_MONGO_MAX_WAIT_TIME, TimeUnit.MILLISECONDS);
           pool.maxConnectionLifeTime(COSMOS_MONGO_MAX_LIFE_TIME_MS, TimeUnit.MILLISECONDS);
@@ -27,6 +33,28 @@ public class MongoConfig {
           socket.readTimeout(COSMOS_MONGO_SOCKET_TIMEOUT_MS, TimeUnit.MILLISECONDS);
           socket.connectTimeout(COSMOS_MONGO_SOCKET_TIMEOUT_MS, TimeUnit.MILLISECONDS);
         });
+  }
+
+  @Slf4j
+  private static final class HeartBeatLogger implements ServerMonitorListener {
+
+    @Override
+    public void serverHearbeatStarted(ServerHeartbeatStartedEvent event) {
+      ServerMonitorListener.super.serverHearbeatStarted(event);
+      log.info(event.toString());
+    }
+
+    @Override
+    public void serverHeartbeatSucceeded(ServerHeartbeatSucceededEvent event) {
+      ServerMonitorListener.super.serverHeartbeatSucceeded(event);
+      log.info(event.toString());
+    }
+
+    @Override
+    public void serverHeartbeatFailed(ServerHeartbeatFailedEvent event) {
+      ServerMonitorListener.super.serverHeartbeatFailed(event);
+      log.info(event.toString());
+    }
   }
 }
 
