a968f820748d43debaa26d3621bfb8c8a687cfbe
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-05-09 15:35:43 +0200

    chore: [RTD-2480] use AutoConfigurationCustomizerProvider as exporter

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/telemetry/AppInsightConfig.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/telemetry/AppInsightConfig.java
@@ -3,17 +3,17 @@ package it.gov.pagopa.rtd.ms.rtdmssenderauth.telemetry;
 import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.mongo.v3_1.MongoTelemetry;
-import io.opentelemetry.sdk.logs.export.LogRecordExporter;
-import io.opentelemetry.sdk.metrics.export.MetricExporter;
-import io.opentelemetry.sdk.trace.export.SpanExporter;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configuration for AppInsight OpenTelemetry.
- * Connection string will automatically take from environment variable APPLICATIONINSIGHTS_CONNECTION_STRING
+ * Configuration for AppInsight OpenTelemetry. Connection string will automatically take from
+ * environment variable APPLICATIONINSIGHTS_CONNECTION_STRING
  */
 @Configuration
 @ConditionalOnProperty(value = "applicationinsights.enabled", havingValue = "true", matchIfMissing = false)
@@ -21,23 +21,19 @@ public class AppInsightConfig {
 
   private final AzureMonitorExporterBuilder azureMonitorExporterBuilder;
 
-  public AppInsightConfig() {
-    this.azureMonitorExporterBuilder = new AzureMonitorExporterBuilder();
+  public AppInsightConfig(
+      @Value("${applicationinsights.connection-string}") String applicationInsights) {
+    this.azureMonitorExporterBuilder = new AzureMonitorExporterBuilder().connectionString(
+        applicationInsights);
   }
 
   @Bean
-  public SpanExporter azureSpanProcessor() {
-    return azureMonitorExporterBuilder.buildTraceExporter();
-  }
-
-  @Bean
-  public MetricExporter azureMetricExporter() {
-    return azureMonitorExporterBuilder.buildMetricExporter();
-  }
-
-  @Bean
-  public LogRecordExporter azureLogRecordExporter() {
-    return azureMonitorExporterBuilder.buildLogRecordExporter();
+  public AutoConfigurationCustomizerProvider otelCustomizer() {
+    return p -> {
+      if (p instanceof AutoConfiguredOpenTelemetrySdkBuilder) {
+        this.azureMonitorExporterBuilder.install((AutoConfiguredOpenTelemetrySdkBuilder) p);
+      }
+    };
   }
 
   @Bean
