668a4af1795d338dff74a6ef9b44d724cdf96120
Michele Fucito <michele.fucito@digitouch.it>
2025-03-13 11:48:13 +0100

    [TAE-361]: update opentelemetry

--- src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/telemetry/AppInsightConfig.java
+++ src/main/java/it/gov/pagopa/rtd/ms/rtdmssenderauth/telemetry/AppInsightConfig.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.rtd.ms.rtdmssenderauth.telemetry;
 
-import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporter;
+import com.azure.monitor.opentelemetry.autoconfigure.AzureMonitorAutoConfigure;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,7 +22,7 @@ public class AppInsightConfig {
   ) {
     return p -> {
       if (p instanceof AutoConfiguredOpenTelemetrySdkBuilder) {
-        AzureMonitorExporter.customize(p, applicationInsights);
+        AzureMonitorAutoConfigure.customize(p, applicationInsights);
       }
     };
   }
